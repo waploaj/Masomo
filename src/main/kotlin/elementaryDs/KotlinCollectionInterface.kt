e7dc008986b3da4b63d2e8f06f9f962391c84530
Abdallah Abdillah <waploaj@icloud.com>
2022-08-03 18:51:23 +0300

    implement mutableIterator on linkedlist

--- src/main/kotlin/elementaryDs/KotlinCollectionInterface.kt
+++ src/main/kotlin/elementaryDs/KotlinCollectionInterface.kt
@@ -10,14 +10,16 @@ import java.lang.IndexOutOfBoundsException
  * Tier 1:- Iterable - provide sequential access of its element via iterator
  * Tier 2:- Collection - provide additional functionality, allowing to check if collection contain particular -
  * element or a collection of element
- * Tier 3:- Mutable - provide mutableiterator, which allow removing item from given collection.
- * Tier 4:- MutableCollection - provide the method to alter collection EXample(add and remove item from collection-
+ * Tier 3:- Mutable - provide MutableIterator, which allow removing item from given collection.
+ * Tier 4:- MutableCollection - provide the method to alter collection example(add and remove item from collection-
  * even clear the entire collection)
  * */
 
 class LinkedListIterator<T>(
     private val list:LinkedList<T>
-):Iterator<T>{
+):Iterator<T>, MutableIterator<T>{
+//If you add this(ie MutableIterator) interface to the list that linkedList is implement.You would see compiler complain again-
+//because iterator doesn't return MutableIterator.
     private var index = 0
     private var lastNode:Node<T>? = null
 
@@ -34,5 +36,15 @@ class LinkedListIterator<T>(
         }
         index++
         return lastNode!!.value
+    }
+
+    override fun remove() {
+        if(index == 1){
+            list.pop()
+        }else{
+            val prevNode = list.nodeAt(index - 2)
+            lastNode = prevNode
+        }
+        index--
     }
 }
